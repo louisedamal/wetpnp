f3373576a013c168e33a83c875a81f708882973a
Louise dAmalric <louise.damalric@gmail.com>
2018-11-26 17:46:40 +0100

    pool CRUD initialized

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :pools, only: [ :index, :new, :create ]
+  resources :pools
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
