1868571f1648c6dbd9bdc31f1576d91090faef3c
Louise dAmalric <louise.damalric@gmail.com>
2018-11-26 16:28:54 +0100

    pools new

--- Gemfile
+++ Gemfile
@@ -30,3 +30,5 @@ group :development, :test do
 end
 
 gem 'devise'
+
+gem 'dotenv-rails', groups: [:development, :test]
--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -2,4 +2,8 @@ class PoolsController < ApplicationController
   def index
     @pools = Pool.all
   end
+
+  def new
+    @pool = Pool.new
+  end
 end
