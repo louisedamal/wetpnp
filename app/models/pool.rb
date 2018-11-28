7e3933d85d5f8056139b1c4065bf4d8ae277976b
Bettina Boon Falleur <bettina.boonfalleur@gmail.com>
2018-11-28 10:51:22 +0100

    added coordinates to pools

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -3,4 +3,6 @@ class Pool < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :title, :address, :description, :capacity, :price, :city, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_152556) do
+ActiveRecord::Schema.define(version: 2018_11_28_094739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2018_11_27_152556) do
     t.datetime "updated_at", null: false
     t.string "photo"
     t.string "city"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_pools_on_user_id"
   end
 
