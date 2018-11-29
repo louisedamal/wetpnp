39f1063eef9e64ee5555223947623b421c2bf741
Louise dAmalric <louise.damalric@gmail.com>
2018-11-29 18:33:47 +0100

    fixing JS bookings

--- app/javascript/components/tab.js
+++ app/javascript/components/tab.js
@@ -1,15 +1,22 @@
 const tabControl = () => {
-  const tabs = document.querySelectorAll('.tabs a');
-
-  tabs.forEach((tab) => {
-    tab.addEventListener('click', (e) => {
-      const contents = document.querySelectorAll('.bookings');
-      tabs.forEach((t) => { t.classList.toggle('active') });
-      contents.forEach((content) => {
-        content.classList.toggle('hidden');
-      });
+  const ownerTab = document.getElementById('owner-trigger');
+  const swimmerTab = document.getElementById('swimmer-trigger');
+  if (ownerTab && swimmerTab) {
+    ownerTab.addEventListener('click', (event) => {
+      event.preventDefault();
+      ownerTab.classList.add('active');
+      swimmerTab.classList.remove('active');
+      document.getElementById('owner').classList.remove('hidden')
+      document.getElementById('swimmer').classList.add('hidden')
     });
-  });
+    swimmerTab.addEventListener('click', (event) => {
+      event.preventDefault();
+      swimmerTab.classList.add('active');
+      ownerTab.classList.remove('active');
+      document.getElementById('swimmer').classList.remove('hidden')
+      document.getElementById('owner').classList.add('hidden')
+    });
+  }
 }
 
 export { tabControl };
