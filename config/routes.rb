228a8a3710c829a765dad19f886d424d66ae55a3
Bettina Boon Falleur <bettina.boonfalleur@gmail.com>
2018-11-29 13:48:21 +0100

    adding a profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+  devise_for :users
+
+  resources :users, only: [:show, :edit, :update]
+
   get '/dashboard', to: 'pages#dashboard'
+
   resources :pools do
     resources :bookings, only: [:new, :create]
   end
+
   resources :bookings, only: [:destroy, :index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
